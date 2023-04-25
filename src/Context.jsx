09d043923c302e961152ccc8dbed8c85e6a39491
adomasr <adomas.razmus@gmail.com>
2023-04-25 03:45:51 +0100

    finished the main page

--- src/Context.jsx
+++ src/Context.jsx
@@ -7,6 +7,7 @@ export const contextObj = createContext();
 
 export function ContextProvider ({children}) {
     const [allPhotos, setAllPhotos] = useState([]);
+    const [cartItems, setCartItems] = useState([]);
     const photoUrl = 'https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json';
 
     useEffect(()=> {
@@ -15,8 +16,31 @@ export function ContextProvider ({children}) {
             .then(data=> setAllPhotos(data));
     },[])
 
+    function toggleLiked(id) {
+        setAllPhotos((prevState)=> {
+            return prevState.map((item)=> {
+                return item.id === id ? {...item, isFavorite: !item.isFavorite} : item;
+            })
+        })
+    }
+
+    function addCartItems(cartItem) {
+        const alreadyInCart = cartItems.includes(cartItem);
+        // if (alreadyInCart) setCartItems((prevState)=> {
+        //     let itemsArray = [...prevState];
+        //     itemsArray.forEach((item, index)=> {
+        //         if (item.id === cartItem.id) itemsArray.splice(index, 1);
+        //     })
+        //     return itemsArray;
+        // })
+        if (alreadyInCart) setCartItems((prevState)=> prevState.filter((item)=> {
+            return item.id !== cartItem.id
+        }))
+        else setCartItems((prevState)=> [...prevState, cartItem]);
+    }
+
     return (
-        <contextObj.Provider value={{allPhotos}}>
+        <contextObj.Provider value={{allPhotos, toggleLiked, addCartItems, cartItems}}>
             {children}
         </contextObj.Provider>
     )
--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,11 +1,41 @@
-import React from "react"
+import React, {useState, useContext} from "react";
+import PropTypes from 'prop-types';
+
+import {contextObj} from '../Context';
+
+
 
 export default function Image({className, imgObj}) {
-    const {url} = imgObj;
+    const [hovered, setHovered] = useState(false);
+    const {url, id, isFavorite} = imgObj;
+    const {toggleLiked, addCartItems, cartItems} = useContext(contextObj);
+    const itemInCart = cartItems.includes(imgObj);
+
+    const heartIcon = <i className={isFavorite ? "ri-heart-fill favorite"
+        : "ri-heart-line favorite"} 
+        onClick={()=> toggleLiked(id)}></i>;
+        
+    const cartIcon = <i onClick={()=> addCartItems(imgObj)} className={itemInCart ? "ri-shopping-cart-fill cart"
+        : "ri-add-circle-line cart"}></i>;
+    
+        
+        
+        function handleHover(event) {
+            if (event.type === 'mouseover') setHovered(true);
+            else setHovered(false);
+        }
+        
+
+        Image.propTypes = {
+            className: PropTypes.string,
+            imgObj: PropTypes.object,
+        }
     
     return (
-        <div className={`${className} image-container`}>
+        <div onMouseOver={handleHover} onMouseLeave={handleHover} className={`${className} image-container`}>
             <img src={url} className="image-grid"/>
+            {hovered && heartIcon}
+            {hovered && cartIcon}
         </div>
     )
 }
